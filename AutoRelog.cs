4f2f58b6513f7ad8b9ebf3f0d60fae61fc61709b
Requi <privat@requi-dev.de>
2020-04-25 18:37:52 +0200

    Update AutoRelog.cs

--- AutoRelog.cs
+++ AutoRelog.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,78 +6,74 @@ using System.Reflection;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
-using VRCModLoader;
-using VRCTools;
 using static UnityEngine.UI.Button;
+using MelonLoader;
 
 namespace AutoRelog
 {
-    [VRCModInfo("AutoRelog", "1.0", "Slaynash")]
-    public class AutoRelog : VRCMod
+    public static class BuildInfo
     {
-        private void OnApplicationStart()
+        public const string Name = "AutoRelog";
+        public const string Author = "Slaynash";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+    
+    public class AutoRelog : MelonMod
+    {
+        public override VRChat_OnUiManagerInit()
         {
-            ModManager.StartCoroutine(VRCToolsSetup());
-        }
-
-        private IEnumerator VRCToolsSetup()
-        {
-            yield return VRCUiManagerUtils.WaitForUiManagerInit();
-
-            VRCModLogger.Log("[AutoRelog] Overwriting login button event");
-            VRCUiPageAuthentication[] authpages = Resources.FindObjectsOfTypeAll<VRCUiPageAuthentication>();
-            VRCUiPageAuthentication loginPage = authpages.First((page) => page.gameObject.name == "LoginUserPass");
-            if (loginPage != null)
+            var authPages = Resources.FindObjectsOfTypeAll<VRCUiPageAuthentication>();
+            VRCUiPageAuthentication loginPage = null;
+            foreach (var authPage in authPages) // I don't trust LINQ + the il2cpp trash
             {
-                Button loginButton = loginPage.transform.Find("ButtonDone (1)")?.GetComponent<Button>();
-                if (loginButton != null)
+                if (authPage.gameObject.name == "LoginUserPass")
                 {
-                    ButtonClickedEvent bce = loginButton.onClick;
-                    loginButton.onClick = new ButtonClickedEvent();
-                    loginButton.onClick.AddListener(() => {
-                        SecurePlayerPrefs.SetString("autorelog_login", GetTextFromUiInputField(loginPage.loginUserName), "vl9u1grTnvXA");
-                        SecurePlayerPrefs.SetString("autorelog_password", GetTextFromUiInputField(loginPage.loginPassword), "vl9u1grTnvXA");
-                        bce?.Invoke();
-                    });
-                    
-                    Transform useprebiousTransform = UnityUiUtils.DuplicateButton(loginButton.transform, "Use Last\nCredentials", new Vector2(440, 0));
-                    useprebiousTransform.GetComponent<RectTransform>().sizeDelta *= 1.8f;
-                    Button useprebiousButton = useprebiousTransform.GetComponent<Button>();
-                    useprebiousButton.onClick = new ButtonClickedEvent();
-                    useprebiousButton.onClick.AddListener(() => {
-                        SetTextToUiInputField(loginPage.loginUserName, SecurePlayerPrefs.GetString("autorelog_login", "vl9u1grTnvXA"));
-                        SetTextToUiInputField(loginPage.loginPassword, SecurePlayerPrefs.GetString("autorelog_password", "vl9u1grTnvXA"));
-                    });
-                    if (!SecurePlayerPrefs.HasKey("autorelog_login"))
-                    {
-                        useprebiousButton.interactable = false;
-                    }
+                    loginPage = authPage;
+                    break;
                 }
-                else
-                    VRCModLogger.Log("[VRCTools] Unable to find login button in login page");
-                
             }
-            else
-                VRCModLogger.Log("[VRCTools] Unable to find login page");
+
+            if (loginPage == null) return;
+            var loginButton = loginPage.transform.Find("ButtonDone (1)")?.GetComponent<Button>();
+            if (loginButton == null) return;
+            
+            var bce = loginButton.onClick;
+            loginButton.onClick = new Button.ButtonClickedEvent();
+            loginButton.onClick.AddListener(new Action(() => {
+                SecurePlayerPrefs.SetString("autorelog_login", GetTextFromUiInputField(loginPage.loginUserName), "vl9u1grTnvXA");
+                SecurePlayerPrefs.SetString("autorelog_password", GetTextFromUiInputField(loginPage.loginPassword), "vl9u1grTnvXA");
+                bce?.Invoke();
+            }));
+
+            var useprebiousTransform = UnityUiUtils.DuplicateButton(loginButton.transform, "Use Last\nCredentials", new Vector2(440, 0));
+            useprebiousTransform.GetComponent<RectTransform>().sizeDelta *= 1.8f;
+            var useprebiousButton = useprebiousTransform.GetComponent<Button>();
+            useprebiousButton.onClick = new Button.ButtonClickedEvent();
+            useprebiousButton.onClick.AddListener(new Action(() => {
+                SetTextToUiInputField(loginPage.loginUserName, SecurePlayerPrefs.GetString("autorelog_login", "vl9u1grTnvXA"));
+                SetTextToUiInputField(loginPage.loginPassword, SecurePlayerPrefs.GetString("autorelog_password", "vl9u1grTnvXA"));
+            }));
+            if (!SecurePlayerPrefs.HasKey("autorelog_login"))
+            {
+                useprebiousButton.interactable = false;
+            }
         }
 
-
-
-        private string GetTextFromUiInputField(UiInputField field)
+        private static string GetTextFromUiInputField(UiInputField field)
         {
             /*
             FieldInfo textField = typeof(UiInputField).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).First(f => f.FieldType == typeof(string) && f.Name != "placeholderInputText");
             return textField.GetValue(field) as string;
             */
-            MethodInfo getTextMethod = typeof(UiInputField).GetMethod("get_text", BindingFlags.Public | BindingFlags.Instance);
-            return getTextMethod.Invoke(field, new object[0]) as string;
+            return field.prop_String_0;
         }
 
-        private void SetTextToUiInputField(UiInputField field, string text)
+        private static void SetTextToUiInputField(UiInputField field, string text)
         {
-            MethodInfo setTextMethod = typeof(UiInputField).GetMethod("set_text", BindingFlags.Public | BindingFlags.Instance);
-            setTextMethod.Invoke(field, new object[] { text });
-            field.GetComponent<InputFieldValidator>()?.Validate(text);
+            field.prop_String_0 = text;
+            field.GetComponent<InputFieldValidator>()?.Method_Public_String_0(text);
         }
     }
 }
